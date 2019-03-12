639643a7cb16c927eb760b46a44412f5fc67160a
MeeraMathews <meeramathewsk@gmail.com>
2019-03-12 13:39:00 +0530

    constructor prgm

---
+++ ConstructorPgm.java
@@ -0,0 +1,19 @@
+public class ConstructorPgm
+{
+
+    int a;
+    ConstructorPgm()
+    {
+        a=100;
+    }
+
+    public static void main(String[] args) 
+    {
+
+        ConstructorPgm ob=new ConstructorPgm();
+        System.out.println(ob.a);
+    }
+}
+
+
+
