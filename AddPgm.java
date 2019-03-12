55b0150169a2102e084d514e504844668b909375
MeeraMathews <meeramathewsk@gmail.com>
2019-03-12 10:57:43 +0530

    addPrgm

---
+++ AddPgm.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class AddPgm
+{
+public static void main(String[] args) {
+    int a,b,c;
+    Scanner myinput =new Scanner(System.in);
+    System.out.println("Enter the first number : ");
+    a=myinput.nextInt();
+    System.out.println("Enter the Second number : ");
+    b=myinput.nextInt();
+    c=a+b;
+    System.out.println("The sum is : "+c);
+
+
+}
+
+
+
+}
